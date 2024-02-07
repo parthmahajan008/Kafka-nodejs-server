5fe9223514b981b3a912aa53a3030006d383acc9
Parth Mahajan <parthmahajan08092003@gmail.com>
2024-02-07 12:32:40 +0530

    handle file buffers

--- index.js
+++ index.js
@@ -1,42 +1,59 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { Kafka } from "kafkajs";
+import fs from 'fs';
 
 const app = express();
-app.use(bodyParser.json())(
-  // KAFKA CONFIG
-  async () => {
-    console.log("Initializing kafka...");
-    const kafka = new Kafka({
-      clientId: "kafka-nodejs-starter",
-      brokers: ["localhost:9092"],
-    });
+app.use(bodyParser.json());
 
-    const consumer = kafka.consumer({ groupId: "demoTopic-consumerGroup" });
+const PORT = process.env.PORT || 3001;
 
-    await consumer
-      .connect()
-      .then(() => console.warn("🚨🚨🚨Connection to consumer is ready"));
+// KAFKA CONFIG
+(async () => {
+  console.log("Initializing kafka...");
+  const kafka = new Kafka({
+    clientId: "kafka-nodejs-starter",
+    brokers: ["localhost:9092"],
+  });
 
-    // KAFKA SUBSCRIPTION
-    const subscribeToTopic = async (params) => {
-      await consumer.subscribe(params);
-    };
-    subscribeToTopic({ topic: "quickstart-events", fromBeginning: true });
+  const consumer = kafka.consumer({ groupId: "demoTopic-consumerGroup" });
 
-    await consumer.run({
-      eachMessage: async ({ topic, partition, message }) => {
-        console.log("Consumed a message = ", {
-          topic,
-          partition,
-          value: message.value.toString(),
-        });
-      },
-    });
+  await consumer.connect();
+  console.warn("🚨🚨🚨Connection to consumer is ready");
 
-    const PORT = process.env.PORT || 3001;
-    app.listen(PORT, () => {
-      console.log(`🎉🎉🎉 Application running on port: ${PORT} 🎉🎉🎉`);
-    });
-  }
-)();
+  // KAFKA SUBSCRIPTION
+  const subscribeToTopic = async (params) => {
+    await consumer.subscribe(params);
+  };
+  subscribeToTopic({ topic: "quickstart-events", fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log("Consumed a message =", {
+        // topic,
+        // partition,
+        // value: message.value.toString(),
+      });
+
+      try {
+        const fileData = JSON.parse(message.value.toString());
+        const { name, type, data } = fileData;
+
+        // Decode base64 data and save the file to disk
+        const decodedData = Buffer.from(data, 'base64');
+        console.log(decodedData)
+        const filePath = `/uploads`;
+
+        fs.writeFileSync(filePath, decodedData);
+
+        console.log(`File "${name}" saved successfully.`);
+      } catch (error) {
+        console.error("Error processing file:", error);
+      }
+    },
+  });
+
+  app.listen(PORT, () => {
+    console.log(`🎉🎉🎉 Application running on port: ${PORT} 🎉🎉🎉`);
+  });
+})();
